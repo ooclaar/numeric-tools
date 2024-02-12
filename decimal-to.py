c93e4f4d7027b62bb5f83ad20b96fbb3ee6db445
ooclaar <ramonalonso@outlook.com.br>
2024-02-12 12:21:40 -0300

    Conversão de textos para inglês e adicionado funcionalidade de Output.

--- decimal-to.py
+++ decimal-to.py
@@ -10,6 +10,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('number', type=int)
     parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-O', '--output', action='store_true')
 
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-b', '--binary', action='store_true')
@@ -18,63 +19,64 @@ def main():
     
     args = parser.parse_args()
 
-    if args.binary == None and args.octal == None and args.hexa == None:
-        print("The attribute is not defined, choose the --binary, --octal, or --hexa parameter.")
+    if args.binary: 
+        divider=2
+    elif args.octal:
+        divider=8
     else:
-        if args.binary: 
-            divisor=2
-        elif args.octal:
-            divisor=8
-        else:
-            divisor=16
+        divider=16
 
-        # Definindo variáveis do sistema para que se possa
-        # possa realizar os calculos do sistema. 
+    # Definindo variáveis do sistema para que se possa
+    # possa realizar os calculos do sistema. 
 
-        resto=0
-        dividendo=args.number
-        debug=args.verbose
-        resultado=""
+    rest=0
+    dividend=args.number
+    debug=args.verbose
+    output=args.output
+    result=""
 
-        while(True):
-            temp = int(dividendo) // int(divisor)
-            resto = int(dividendo) % int(divisor)
-            quociente = int(dividendo) - int(resto)
-            dividendo = temp
+    while(True):
+        temp = int(dividend) // int(divider)
+        rest = int(dividend) % int(divider)
+        quociente = int(dividend) - int(rest)
+        dividend = temp
 
-            # Realizando modificações de alguns números, caso a opção 
-            # hexa tenha sido solicitada. 
+        # Realizando modificações de alguns números, caso a opção 
+        # hexa tenha sido solicitada. 
 
-            if args.hexa:
-                if resto == 10:
-                    resto="A"
-                elif resto == 11:
-                    resto="B"
-                elif resto == 12:
-                    resto="C"
-                elif resto == 13:
-                    resto="D"
-                elif resto == 14:
-                    resto="E"
-                elif resto == 15:
-                    resto="F"
+        if args.hexa:
+            if rest == 10:
+                rest="A"
+            elif rest == 11:
+                rest="B"
+            elif rest == 12:
+                rest="C"
+            elif rest == 13:
+                rest="D"
+            elif rest == 14:
+                rest="E"
+            elif rest == 15:
+                rest="F"
 
-            # Caso habilitado, mostra todas as operações até chegar
-            # no valor do resultado. 
+        # Caso habilitado, mostra todas as operações até chegar
+        # no valor do result. 
 
-            if(debug):
-                
-                print("Valor do dividendo: " + str(dividendo))
-                print("Valor do divisor: " + str(divisor))
-                print("Valor do resto: " + str(resto))
-                print("Valor do quociente: " + str(quociente))
-                print("-------------------------------------")
+        if(debug):
+            
+            print("Dividend value: " + str(dividend))
+            print("Divider value: " + str(divider))
+            print("Rest value: " + str(rest))
+            print("Quotient value: " + str(quociente))
+            print("-------------------------------------")
 
-            resultado = str(resultado) + str(resto)
+        result = str(result) + str(rest)
 
-            if (dividendo==0):
-                print("O valor de " + str(args.number) + " ficou: " + str(resultado)[::-1])
-                break
+        if (dividend==0):
+            if(output):
+                print(str(result))
+            else:
+                print("The value " + str(args.number) + " was: " + str(result)[::-1])
+            break
 
 if __name__ == "__main__":
     main()
