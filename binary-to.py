d7a555a8834cbf02b6f8bd11970c63d9c87d92e1
ooclaar <ramonalonso@outlook.com.br>
2024-02-12 20:28:36 -0300

    Correções do código e otimizações.

--- binary-to.py
+++ binary-to.py
@@ -10,81 +10,132 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('number', type=int)
     parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-O', '--output', action='store_true')
 
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-d', '--decimal', action='store_true')
     group.add_argument('-o', '--octal', action='store_true')
     group.add_argument('-H', '--hexa', action='store_true')
 
+    # Variaveis Globais
+
     args = parser.parse_args()
     number = str(args.number)
-    
+    debug = args.debug
+    output = args.output
+
     if args.decimal:
         
+        # Escopo quando for decimal.
+
+        bitstring = ""
         result = 0
         vposicao = 1
 
         for bit in number[::-1]:
+            
+            bitstring = str(bit)
+            
             if bit=='1':
                 result=result+vposicao
-            elif bit=='0':
-                continue
-            else:
+            elif bit!='0':
                 result=1
                 break
+                
             vposicao=vposicao*2
+
         if result!=0:
             print("The value decimal is: " + str(result))
         else:
             print("The number is not binary.")
     
+    # Verifica qual a opção escolhida e define os ajustes. 
+    
     if args.octal:
+        factor=3
+    else:
+        factor=4
+    
+    # Escopo de variáveis locais
+
+    resultin = 0
+    resultout = ""
+    vposicao = 1
+    bitstring = ""
+    
+    while (True):
+        number = "0" + str(number)
+        if ((len(number) % factor)==0):
+            if debug:
+                print("O número passado foi modificado para: {number}.")
+            break
+
+    for i in range(0, len(number), factor):
         
-        resultin = 0
-        resultout = ""
-        vposicao = 1
-        bitstring = ""
-        rest = 0
-        diff = 0
-
-        rest = len(number) % 3
-        while (True):
-            number = "0" + str(number)
-            if ((len(number) % 3)==0):
-                print("A string foi modificada para: " + str(number))
-                break
-
-        for i in range(0, len(number), 3):
-            
-            part = str(number[::-1][i:i+3])
+        part = str(number[::-1][i:i+factor])
+        
+        if debug:
             print("Parte: " + part)
 
-            for bit in part:
+        for bit in part:
+            
+            bitstring = str(bit)
+            
+            if debug:
+                print("Valor do Bit: " + str(bitstring))
+                print("Valor da Posição:" + str(vposicao))
+            
+            if bit=='1':
+                resultin=int(resultin) + (int(vposicao) * int(bit))
+            elif bit!='0':
+                result=1
+                break
                 
-                bitstring = str(bit)
-                
-                print("BIT: " + str(bitstring))
-                print("VPO:" + str(vposicao))
-                
-                if bit=='1':
-                    resultin=int(resultin) + (int(vposicao) * int(bit))
-                elif bit!='0':
-                    result=1
-                    break
-                    
-                vposicao=vposicao*2
+            vposicao=vposicao*2
 
+        if debug:
             print("Soma do Bloco: " + str(resultin))
             print("------------------------------------")
-            
-            vposicao = 1
-            resultout = str(resultout) + str(resultin)
-            resultin = 0
 
-        if resultout!=0:
-            print("The value octal is: " + str(resultout)[::-1])
+        if args.octal:         
+            resultout = str(resultout) + str(resultin)
         else:
-            print("The number is not binary.")    
+            
+            # Troca valores para Hexadecimal quando superior a 10.
+
+            if resultin == 10:
+                resultin = "A"
+            elif resultin == 11:
+                resultin = "B"
+            elif resultin == 12:
+                resultin = "C"
+            elif resultin == 13:
+                resultin = "D"
+            elif resultin == 14:
+                resultin = "E"
+            elif resultin == 15:
+                resultin = "F"
+
+            resultout = str(resultout) + str(resultin)
+
+        resultin = 0
+        vposicao = 1
+
+    # Verifica os dados compilados e mostra o resultado. 
+
+    if resultout!=0:
+        if args.octal:
+            if output:
+                print(str(resultout)[::-1])
+            else:
+                print("The value octal is: " + str(resultout)[::-1])
+        else:
+            if output:
+                print(str(resultout)[::-1])
+            else:
+                print("The value hexa is: " + str(resultout)[::-1])
+    else:
+        print("The number is not binary.")
     
 if __name__ == "__main__":
     main()
